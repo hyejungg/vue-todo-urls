d592b74c3b9a5dda7f47bdac50447ab2d1eedd2e
hyejungg <kimhyejung12@naver.com>
2023-09-21 01:01:56 +0900

    fix: 애니메이션 긴급 막기(추후에 수정하기)

--- src/components/web/Presentation.vue
+++ src/components/web/Presentation.vue
@@ -1,24 +1,32 @@
 <template>
     <div id="presentation-web">
-        <div class="animate-element">
+        <div class="dummy" />
+        <div class="animate-element" data-scroll>
             <h1>안녕하세요. 몰리입니다. (ouo)</h1>
         </div>
-        <div class="animate-element">
-            <h1>사실 애니메이션을 적용하려고 했는데 잘 안되네요 (ㅠㅛㅠ)</h1>
-        </div>
-        <div class="animate-element">
+        <div class="dummy" />
+        <div class="animate-element" data-scroll>
             <h1>이번에는 DB 없는 체크리스트를 만들었습니다.</h1>
         </div>
-        <div class="animate-element"><h1>아이디어의 원천은</h1></div>
-        <div class="animate-element">
+        <div class="dummy" />
+        <div class="animate-element" data-scroll>
+            <h1>아이디어의 배경은</h1>
+        </div>
+        <div class="dummy" />
+        <div class="animate-element" data-scroll>
             <img width="80%" src="../../assets/ppt_img1.png" />
             <a href="https://www.youtube.com/watch?v=pCOBmmJARPE"
                 ><p>코딩애플 유튜브 링크</p></a
             >
         </div>
-        <div class="animate-element"><h1>너무 식상한가요?</h1></div>
-        <div class="animate-element">
-            <h1>저는 이런 생각은 못했었습니다.</h1>
+        <div class="dummy" />
+        <div class="animate-element" data-scroll><h1>너무 식상한가요?</h1></div>
+        <div class="dummy" />
+        <div class="animate-element" data-scroll>
+            <h1>저는 이런 생각은 못했습니다. (ㅎ^ㅎ)</h1>
+        </div>
+        <div class="dummy" />
+        <div class="animate-element" data-scroll>
             <br />
             <br />
             <h1>LocalStorage X</h1>
@@ -29,8 +37,12 @@
             <br />
             <h1 style="color: green">데이터는 오직 URL 보관</h1>
         </div>
-        <div class="animate-element"><h1>배운 점은 다음과 같습니다.</h1></div>
-        <div class="animate-element">
+        <div class="dummy" />
+        <div class="animate-element" data-scroll>
+            <h1>배운 점은 다음과 같습니다.</h1>
+        </div>
+        <div class="dummy" />
+        <div class="animate-element" data-scroll>
             <h1>1. netlify 를 이용한 FE 배포</h1>
             <br />
             <br />
@@ -45,7 +57,8 @@
             <br />
             <h2 style="color: green">서버 영역에 대한 백엔드 배포는 불가능</h2>
         </div>
-        <div class="animate-element">
+        <div class="dummy" />
+        <div class="animate-element" data-scroll>
             <h1>2. 반응형에 대한 고민</h1>
             <br />
             <br />
@@ -61,50 +74,81 @@
             <img width="40%" src="../../assets/reative_code1.png" />
             <img width="40%" src="../../assets/reative_code2.png" />
         </div>
-        <div class="animate-element">
-            <a href="/"><h1>진짜로 공개합니다.</h1></a>
+        <div class="dummy" />
+        <div class="animate-element" data-scroll>
+            <h1>3. 발표자료에 스크롤 애니메이션 적용</h1>
+            <br /><br />
+            <h4>사실 애니메이션을 적용하려고 했는데 잘 안되네요 (ㅠㅛㅠ)</h4>
         </div>
+        <div class="dummy" />
+        <div class="animate-element" data-scroll>
+            <a href="/"><h1>진짜로 공개합니다. (ouo)</h1></a>
+        </div>
+        <div class="dummy" />
     </div>
 </template>
 <script>
+import ScrollOut from "scroll-out";
+
 export default {
     name: "PresentationMobile",
     data: () => ({
         observer: null,
     }),
     mounted() {
-        this.observer = new IntersectionObserver((entries) => {
-            entries.forEach((element) => {
-                // element 가 화면에 보일 때
-                if (element.isIntersecting) {
-                    console.log(element);
-                    element.target.style = "opacity: 1 !important;";
-                }
-            });
-        });
-        const divElements = document.querySelectorAll(".animate-element");
-        divElements.forEach((element) => {
-            this.observer.observe(element);
+        this.so = ScrollOut({
+            scope: this.$el,
         });
     },
+    destroyed() {
+        this.so.teardown();
+    },
+    // mounted() {
+    //     this.observer = new IntersectionObserver((entries) => {
+    //         entries.forEach((element) => {
+    //             // element 가 화면에 보일 때
+    //             if (element.isIntersecting) {
+    //                 console.log(element);
+    //                 element.target.attributes = "data-scroll";
+    //             }
+    //         });
+    //     });
+    //     const divElements = document.querySelectorAll(".animate-element");
+    //     divElements.forEach((element) => {
+    //         this.observer.observe(element);
+    //     });
+    // },
 };
 </script>
 <style scoped>
 #presentation-web {
-    background: black !important;
+    background: black;
     height: auto;
     overflow: auto;
 }
 
-div {
+.dummy {
     padding-top: 800px;
     padding-bottom: 800px;
+}
+
+.animate-element {
     color: white;
     text-align: center;
-    opacity: 0;
+    /* opacity: 0; */
 }
 
 a {
     text-decoration-line: none;
+}
+
+[data-scroll] {
+    transition: opacity 1s;
+}
+[data-scroll="in"] {
+    opacity: 1;
+}
+[data-scroll="out"] {
+    opacity: 0;
 }
 </style>
